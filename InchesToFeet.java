723c3c35c8aa1e54d64be163eb0fa0c63fbcfe9d
Kathy-Zeng <70964239+Kathy-Zeng@users.noreply.github.com>
2020-09-17 16:42:40 -0700

    Update InchesToFeet.java

--- InchesToFeet.java
+++ InchesToFeet.java
@@ -1,25 +1,27 @@
 // Kathy Zeng
 // 9/17/20
 // This program gives me inches to feet conversion.
-1. Input strings
-2. Enter the inches
-3. Convert from inches to feet
+// InchesToFeet.java
+1. Initialize and declare variables 
+2. Input strings 
+3. Convert from inches to feet 
 
 import java.util.Scanner;
-InchesToFeet.java
 public class InchesToFeet;
 {
   public static void main(String[] args);
   {
+   int inches;
+	 double feet;
+	 
    Scanner src = new Scanner(System.in); 
-   
-   System.out.println("Please enter the number of inches: ");
-   nextInt();
+   System.out.println("Please enter the number of inches: "); 
+   inches = input.nextInt(); 
    System.out.printf("Inches %.2f"); 
-   nextFloat();
- 
-    System.out.println("Please enter the number of feet: ");
-    nextDouble(); 
-
+   inches = input.nextFloat(); 
+   feet = inches/12; 
+   System.out.println("Please enter the number of feet: ");
+   feet = input.nextDouble(); 
+   System.out.println(inches + "\" equals " + feet + "\' "); 
   }
 }
