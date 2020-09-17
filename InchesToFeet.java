e4665591490536349b52e2a606ff785d8019056b
Kathy-Zeng <70964239+Kathy-Zeng@users.noreply.github.com>
2020-09-17 15:18:01 -0700

    Update and rename CalculatePi.java to InchesToFeet.java

---
+++ InchesToFeet.java
@@ -0,0 +1,25 @@
+// Kathy Zeng
+// 9/17/20
+// This program gives me inches to feet conversion.
+1. Input strings
+2. Enter the inches
+3. Convert from inches to feet
+
+import java.util.Scanner;
+InchesToFeet.java
+public class InchesToFeet;
+{
+  public static void main(String[] args);
+  {
+   Scanner src = new Scanner(System.in); 
+   
+   System.out.println("Please enter the number of inches: ");
+   nextInt();
+   System.out.printf("Inches %.2f"); 
+   nextFloat();
+ 
+    System.out.println("Please enter the number of feet: ");
+    nextDouble(); 
+
+  }
+}
